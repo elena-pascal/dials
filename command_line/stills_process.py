9a02a1188bc318a484c227bc6f92565621b8e62b
Aaron Brewster <asbrewster@lbl.gov>
2020-12-11 16:32:32 -0800

    Tidy

--- command_line/stills_process.py
+++ command_line/stills_process.py
@@ -344,8 +344,6 @@ class Script(object):
         if self.params.input.reference_geometry is None:
             return
 
-        from dxtbx.model.experiment_list import ExperimentListFactory
-
         try:
             ref_experiments = ExperimentListFactory.from_json_file(
                 self.params.input.reference_geometry, check_format=False
@@ -796,7 +794,6 @@ class Processor(object):
 
         if params.output.composite_output:
             assert composite_tag is not None
-            from dxtbx.model.experiment_list import ExperimentList
 
             self.all_imported_experiments = ExperimentList()
             self.all_strong_reflections = flex.reflection_table()
@@ -1229,7 +1226,6 @@ The detector is reporting a gain of %f but you have also supplied a gain of %f. 
                 refls = indexed.select(indexed['id']==expt_id)
                 refls['id'] = flex.int(len(refls), len(new_experiments))
                 #refls.reset_ids()
-                #import ipdb;ipdb.set_trace()
                 del refls.experiment_identifiers()[expt_id]
                 refls.experiment_identifiers()[len(new_experiments)] = expt.identifier
                 new_reflections.extend(refls)
@@ -1276,8 +1272,6 @@ The detector is reporting a gain of %f but you have also supplied a gain of %f. 
                 )()
 
         if self.params.significance_filter.enable:
-            #from dxtbx.model.experiment_list import ExperimentList
-
             from dials.algorithms.integration.stills_significance_filter import (
                 SignificanceFilter,
             )
